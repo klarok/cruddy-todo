b3798fbebb658243ae5c40afada57319e72c15b9
klarok <karaylu@gmail.com>
2018-12-04 10:52:36 -0800

    finished tests starting to refactor readall

--- datastore/index.js
+++ datastore/index.js
@@ -41,24 +41,29 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  let filePath = path.join(exports.dataDir, `${id}.txt`);
+  fs.readFile(filePath, (err, fileData) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      fs.writeFile(filePath, text, (err) => {
+        if (err) {
+          throw err;
+        }
+        callback(err, {id, text});
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(err, id);
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
